6494f56ae85e15ad2647f09e43c9cf2993f5172d
JamesLorencee <james.seril.12@gmail.com>
2023-03-12 18:43:01 +0800

    Added: RegisterActivity.kt HomeActivity.kt activity_register.xml activity_home.xml background.xml bluebutton.xml styles.xml
    Changed:
    Gradle to bindfeature to true
    
    UI of Login page and Register Page

--- app/src/main/java/ph/edu/dlsu/mobdeve/seril/james/weatherplan/MainActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/seril/james/weatherplan/MainActivity.kt
@@ -1,11 +1,35 @@
 package ph.edu.dlsu.mobdeve.seril.james.weatherplan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import ph.edu.dlsu.mobdeve.seril.james.weatherplan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+            val username = binding.etEmailAddress.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            if (username == "admin" && password == "admin") {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Invalid Username or Password", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.registerBtn.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/seril/james/weatherplan/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package ph.edu.dlsu.mobdeve.seril.james.weatherplan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
