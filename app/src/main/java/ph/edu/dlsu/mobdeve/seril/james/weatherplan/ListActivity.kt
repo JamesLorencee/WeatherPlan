11685f05a1c60f8414ed34cac6700e0be66d89f0
JamesLorencee <james.seril.12@gmail.com>
2023-03-28 18:41:55 +0800

    Added: activity_list.xml item_list.xml ListActivity.kt

---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/seril/james/weatherplan/ListActivity.kt
@@ -0,0 +1,11 @@
+package ph.edu.dlsu.mobdeve.seril.james.weatherplan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
