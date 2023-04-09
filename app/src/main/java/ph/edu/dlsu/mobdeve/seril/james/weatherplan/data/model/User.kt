a2e8d30bd2407486697c39ef490f400393e08804
Anton de Leon <iamgpmc@gmail.com>
2023-04-09 16:13:13 +0800

    added user class

---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/seril/james/weatherplan/data/model/User.kt
@@ -0,0 +1,10 @@
+package ph.edu.dlsu.mobdeve.seril.james.weatherplan.data.model
+
+class User () {
+    var uid: String? = null
+    var email: String? = null
+    var username: String? = null
+    val scheduleList: ArrayList<Schedule> = ArrayList<Schedule>()
+
+    constructor(uid: String?, email: String?, username: String?, scheduleList: ArrayList<Schedule>): this()
+}
